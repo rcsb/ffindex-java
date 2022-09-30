dac9182cefcb3d41c029c14c587164bf37afe314
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2022-09-30 10:46:33 -0700

    cleanup

--- ffindex-java-core/src/test/java/org/rcsb/ffindex/impl/ReadWriteFileBundleTest.java
+++ ffindex-java-core/src/test/java/org/rcsb/ffindex/impl/ReadWriteFileBundleTest.java
@@ -11,7 +11,6 @@ import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
