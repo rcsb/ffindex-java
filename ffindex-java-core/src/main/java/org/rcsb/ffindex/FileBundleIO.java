50182f55919e8652e5a38acb1f5c6a6588f8c097
Sebastian Bittrich <sebastian.bittrich@rcsb.org>
2022-10-13 13:02:44 -0700

    cleanup

--- ffindex-java-core/src/main/java/org/rcsb/ffindex/FileBundleIO.java
+++ ffindex-java-core/src/main/java/org/rcsb/ffindex/FileBundleIO.java
@@ -16,7 +16,6 @@ import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
